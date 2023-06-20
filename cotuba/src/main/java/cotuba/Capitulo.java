ca928dac503002f50058f906a6ff808a29bdd3c7
ramilani12 <ramilani@gmail.com>
2023-06-19 21:23:06 -0300

    SRP separacao das classes geradoras

---
+++ cotuba/src/main/java/cotuba/Capitulo.java
@@ -0,0 +1,26 @@
+package cotuba;
+
+import java.io.Serializable;
+
+public class Capitulo implements Serializable {
+
+    private String titulo;
+
+    private String conteudoHTML;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getConteudoHTML() {
+        return conteudoHTML;
+    }
+
+    public void setConteudoHTML(String conteudoHTML) {
+        this.conteudoHTML = conteudoHTML;
+    }
+}
